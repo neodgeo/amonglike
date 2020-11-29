a16bf614fc3e5f5c757a21da55fcb295ae9f2e9a
Kizaret <kizaret@assystem.com>
2020-11-29 01:15:40 +0100

    New Fq Input System

--- client/pages/fraise-epreuve1.js
+++ client/pages/fraise-epreuve1.js
@@ -31,25 +31,18 @@ Template.fraiseEpreuve1.onRendered(function(){
 })
 
 Template.fraiseEpreuve1.events({
-    'click #scenef1': function (event, template) {
-        var instance = Template.instance();
-        if (pecheColorData.find().fetch()[0].activeColor == 'red'){
-          pecheColorData.update({_id:instance.pecheId.get()},{$set:{activeColor:'yellow'}})
-          fraiseColorData.update({_id:instance.fraiseId.get()},{$set:{activeColor:'yellow'}})
-          console.log('red to yellow')
-        } else if (pecheColorData.find().fetch()[0].activeColor == 'yellow'){
-          pecheColorData.update({_id:instance.pecheId.get()},{$set:{activeColor:'red'}})
-          fraiseColorData.update({_id:instance.fraiseId.get()},{$set:{activeColor:'red'}})
-          console.log('yellow to red')
-        } else {
-          console.log('?')
-        }
-        var perso = document.querySelector('#persof1');
 
-        // perso.src = "img/persomove.png";
-        // perso.style.marginLeft = "240px"
+    'click #fq600': function (event, template) {
+      var instance = Template.instance();
+        pecheColorData.update({_id:instance.pecheId.get()},{$set:{activeColor:'yellow'}})
+        fraiseColorData.update({_id:instance.fraiseId.get()},{$set:{activeColor:'yellow'}})
+    },
 
-      },
+    'click #fq700': function (event, template) {
+      var instance = Template.instance();
+        pecheColorData.update({_id:instance.pecheId.get()},{$set:{activeColor:'red'}})
+        fraiseColorData.update({_id:instance.fraiseId.get()},{$set:{activeColor:'red'}})
+    },
 })
 
 Template.fraiseEpreuve1.destroyed = function(){
